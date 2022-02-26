2b4d06cedfe61ea7fdf4f4850789656c2ef27dc1
sunmin <sunm309@naver.com>
2022-02-27 02:07:28 +0900

    remove source files

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "[JavaScript] 게시글 제목 테스트",
-        "excerpt":"테스트 게시글 내용제목      ㅋㅋㅋㅋㅋ ㅋㅋㅋㅋㅋㅋ   맨 위로 이동하기  ","categories": ["JavaScript"],
-        "tags": ["Programming","Test","js","react"],
-        "url": "/javascript/test/",
-        "teaser": null
-      }]
